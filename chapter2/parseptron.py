8b6d4f6a41187a7e6c68486282e706c89d29b743
pokotyamu <pokotyamu@gmail.com>
2018-07-31 19:35:46 +0900

    OR method

--- chapter2/parseptron.py
+++ chapter2/parseptron.py
@@ -10,6 +10,15 @@ def AND(x1, x2):
     else:
         return 1
 
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.3
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
 print("[AND]")
 print(AND(0,0))
@@ -17,10 +26,8 @@ print(AND(0,1))
 print(AND(1,0))
 print(AND(1,1))
 
-"""
 print("[OR]")
-print(AND(0,0))
-print(AND(0,1))
-print(AND(1,0))
-print(AND(1,1))
-"""
+print(OR(0,0))
+print(OR(0,1))
+print(OR(1,0))
+print(OR(1,1))
