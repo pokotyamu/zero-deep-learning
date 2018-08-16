c35b04c5921d1bbacffe63eb2633990c74bc824d
pokotyamu <pokotyamu@gmail.com>
2018-08-16 17:31:12 +0900

    二乗和誤差

---
+++ ch04/loss_function.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t)** 2)
+
+y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+print(mean_squared_error(y, t))
