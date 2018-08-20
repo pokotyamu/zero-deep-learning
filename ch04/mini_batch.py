6e1f968567bca250e311b63a1f02ac30a04a7ec4
pokotyamu <pokotyamu@gmail.com>
2018-08-20 19:23:09 +0900

    バッチ対応型交差エントロピー

--- ch04/mini_batch.py
+++ ch04/mini_batch.py
@@ -9,3 +9,21 @@ batch_mask = np.random.choice(x_train.shape[0], 10)
 print(x_train.shape)
 print(t_train.shape)
 print(batch_mask)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y + 1e-7)) / batch_size
+
+y = np.array([
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0],
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.7, 0.0, 0.0],
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0],
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0],
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]])
+batch = np.arange(5)
+"""one-hot だと 0/1 だから、正解ラベル以外は掛けた値が0なので、正解ラベルだけ抽出すればよい"""
+t = np.array([2,7,0,9,4])
