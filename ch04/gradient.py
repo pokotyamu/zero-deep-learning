a534993010f4235066dc86285221d5b17b37ff78
pokotyamu <pokotyamu@gmail.com>
2018-08-21 18:28:03 +0900

    勾配

---
+++ ch04/gradient.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def function2(x):
+    return x[0]**2 + x[1]**2
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)  # x と同じ形状の配列を生成
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+print(numerical_gradient(function2, np.array([3.0, 4.0])))
