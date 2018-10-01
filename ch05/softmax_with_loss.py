8dcfa521a65779a81f07120d1cebd177f8d9969a
pokotyamu <pokotyamu@gmail.com>
2018-10-01 16:56:55 +0900

    SoftmaxWithLoss 関数

---
+++ ch05/softmax_with_loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def soft_max(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = soft_max(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
